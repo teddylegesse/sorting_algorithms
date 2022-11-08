6b048f0bf1e18cc6f5a4a259f46e424601129f69
teddylegesse <teddylegesse334@gmail.com>
2022-11-08 11:35:17 +0000

    second commit

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,8 +16,8 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 
 void bubble_sort(int *array, size_t size);
 
